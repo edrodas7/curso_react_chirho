7983542dd347946366c687d15352663224edd58d
Edgar-Chirho <edgar-rodas@praind.com>
2026-05-10 19:10:01 -0600

    HALLELUJAH primer commit chirho HALELLUJAH

---
+++ react_app_chirho/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+    </Routes>
+  );
+}
+
+
+export default App;
